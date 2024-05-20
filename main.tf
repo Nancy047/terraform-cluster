5b20fc619f742f5b93a94286a10539db52a10bb7
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-20 16:33:20 +0530

    update

--- main.tf
+++ main.tf
@@ -1,15 +1,48 @@
-# Create the GKE cluster
-resource "google_container_cluster" "cluster" {
-  name               = var.cluster_name
-  location           = var.region
-  initial_node_count = var.initial_node_count
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "demo-cluster"
+  location = "us-central1"
+  project  = "lumen-b-ctl-047"
 
   remove_default_node_pool = true
+  initial_node_count       = 1
 
-  # Node pool configuration
-  node_pool {
-    name         = "default-pool"
-    machine_type = var.node_machine_type
-    node_count   = var.initial_node_count
+  networking_mode = "VPC_NATIVE"
+
+  workload_identity_config {
+    workload_pool = "lumen-b-ctl-047.svc.id.goog"
+  }
+}
+
+resource "google_container_node_pool" "pool1" {
+  name       = "pool-1"
+  location  = "us-central1"
+  project   = "lumen-b-ctl-047"
+  cluster   = google_container_cluster.primary.name
+  node_count = 1
+
+  autoscaling {
+    enabled = false
+  }
+}
+
+resource "google_container_node_pool" "pool2" {
+  name       = "pool-2"
+  location  = "us-central1"
+  project   = "lumen-b-ctl-047"
+  cluster   = google_container_cluster.primary.name
+  node_count = 1
+
+  autoscaling {
+    enabled = false
   }
 }
