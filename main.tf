dff7ee2640bb29449c62f57a08c4ea38aeb416f8
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-20 23:28:21 +0530

    update

--- main.tf
+++ main.tf
@@ -9,39 +9,43 @@ terraform {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "demo-cluster"
-  location = "us-central1"
- project  = "lumen-b-ctl-047"
+  name     = "test-cluster"
+  location = "us-central1-a"
+ project  = "sampleproject"
 
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  networking_mode = "k8s_pod_networking"
+  networking_mode = "k8s_ipo_alloc"
 
   ip_allocation_policy {
-    cluster_ipv4_cidr_block  = "/14"
-    services_ipv4_cidr_block = "/20"
   }
 }
 
-resource "google_container_node_pool" "pool_1" {
+resource "google_container_node_pool" "pool1" {
   name       = "pool-1"
-  location  = "us-central1"
-  project   = "lumen-b-ctl-047"
+  location  = "us-central1-a"
+  project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-  node_count = 1
+
+  node_config {
+    machine_type = "n1-standard-1"
+  }
 
   autoscaling {
     enabled = false
   }
 }
 
-resource "google_container_node_pool" "pool_2" {
+resource "google_container_node_pool" "pool2" {
   name       = "pool-2"
-  location  = "us-central1"
-  project   = "lumen-b-ctl-047"
+  location  = "us-central1-a"
+  project   = "sampleproject"
   cluster   = google_container_cluster.primary.name
-  node_count = 1
+
+  node_config {
+    machine_type = "n1-standard-2"
+  }
 
   autoscaling {
     enabled = false
