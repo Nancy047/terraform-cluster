27a98df5c099679af67ad0c51907548af5b80e77
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-20 19:56:30 +0530

    update

--- main.tf
+++ main.tf
@@ -13,46 +13,41 @@ resource "google_container_cluster" "primary" {
   location = "us-central1-a"
   project  = "lumen-b-ctl-047"
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
   networking_mode = "VPC_NATIVE"
+  initial_node_count = 1
 
-  workload_identity_config {
-    workload_pool = "lumen-b-ctl-047.svc.id.goog"
-  }
-}
-
-resource "google_container_node_pool" "pool1" {
-  name       = "pool-1"
-  location  = "us-central1-a"
-  project   = "lumen-b-ctl-047"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
+  ip_allocation_policy {
   }
 
-  management {
-    auto_repair  = true
-    auto_upgrade = true
+  remove_default_node_pool = true
+
+  node_pool {
+    name       = "default-pool"
+    node_count = 1
+    config {
+      machine_type = "e2-medium"
+      disk_size_gb = 100
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ]
+    }
   }
-}
 
-resource "google_container_node_pool" "pool2" {
-  name       = "pool-2"
-  location  = "us-central1-a"
-  project   = "lumen-b-ctl-047"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
+  node_pool {
+    name       = "secondary-pool"
+    node_count = 1
+    config {
+      machine_type = "e2-standard-4"
+      disk_size_gb = 200
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ]
+    }
   }
 }
