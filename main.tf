9ed4d84e06893787989e2f5bfca12f909d36e028
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-21 19:15:23 +0530

    update

--- main.tf
+++ main.tf
@@ -3,74 +3,19 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "5.22.0"
     }
   }
 }
 
-resource "google_container_cluster" "primary" {
-  name     = "demo-cluster"
-  location = "us-central1"
-  project  = "demo-project"
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  networking_mode = "VPC_NATIVE"
-
-  workload_identity_config {
-    workload_pool = "demo-project.svc.id.goog"
-  }
+provider "google" {
+  credentials = "path/to/credentials.json"
+  project     = "lumen-b-ctl-047"
 }
 
-resource "google_container_node_pool" "pool1" {
-  name       = "pool-1"
-  location  = "us-central1"
-  project   = "demo-project"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
-  }
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-}
-
-resource "google_container_node_pool" "pool2" {
-  name       = "pool-2"
-  location  = "us-central1"
-  project   = "demo-project"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = false
-  }
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-  node_config {
-    machine_type = "n1-standard-1"
-    disk_size_gb = 100
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
+resource "google_storage_bucket" "my-bucket" {
+  name          = "my-bucket"
+  location      = "US-CENTRAL1"
+  storage_class = "STANDARD"
+ uniform_bucket_level_access = true
 }
