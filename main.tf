20d36262f682b06b15e7fca3b94c20c2c457a84d
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-21 13:01:46 +0530

    update

--- main.tf
+++ main.tf
@@ -11,25 +11,40 @@ terraform {
 resource "google_container_cluster" "primary" {
   name     = "demo-cluster"
   location = "us-central1"
- project  = "demo-project"
-
-  node_pool {
-    name       = "default-pool"
-    node_count = 1
-
-    autoscaling {
-      enabled = false
-    }
-  }
-
-  node_pool {
-    name       = "secondary-pool"
-    node_count = 1
-
-    autoscaling {
-      enabled = false
-    }
-  }
+  project  = "demo-project"
 
   remove_default_node_pool = true
+  initial_node_count       = 1
+
+  networking_mode = "VPC_NATIVE"
+
+  workload_identity_config {
+    workload_pool = "demo-project.svc.id.goog"
+  }
+}
+
+resource "google_container_node_pool" "pool1" {
+  name       = "pool-1"
+  location  = "us-central1"
+  project   = "demo-project"
+  cluster   = google_container_cluster.primary.name
+
+  node_count = 1
+
+  autoscaling {
+    enabled = false
+  }
+}
+
+resource "google_container_node_pool" "pool2" {
+  name       = "pool-2"
+  location  = "us-central1"
+  project   = "demo-project"
+  cluster   = google_container_cluster.primary.name
+
+  node_count = 1
+
+  autoscaling {
+    enabled = false
+  }
 }
