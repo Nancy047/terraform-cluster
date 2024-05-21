5d7712ceba9812abed823cf13f76693a401a19ea
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-21 13:01:38 +0530

    update

--- main.tf
+++ main.tf
@@ -11,33 +11,25 @@ terraform {
 resource "google_container_cluster" "primary" {
   name     = "demo-cluster"
   location = "us-central1"
-  project  = "demo-project"
+ project  = "demo-project"
+
+  node_pool {
+    name       = "default-pool"
+    node_count = 1
+
+    autoscaling {
+      enabled = false
+    }
+  }
+
+  node_pool {
+    name       = "secondary-pool"
+    node_count = 1
+
+    autoscaling {
+      enabled = false
+    }
+  }
 
   remove_default_node_pool = true
-  initial_node_pools {
-    name             = "default-pool"
-    initial_node_count = 1
-  }
-}
-
-resource "google_container_node_pool" "pool1" {
-  name       = "pool-1"
-  location  = "us-central1"
-  cluster   = google_container_cluster.primary.name
-  project    = "demo-project"
-  node_count = 1
-  autoscaling {
-    enabled = false
-  }
-}
-
-resource "google_container_node_pool" "pool2" {
-  name       = "pool-2"
-  location  = "us-central1"
-  cluster   = google_container_cluster.primary.name
-  project    = "demo-project"
-  node_count = 1
-  autoscaling {
-    enabled = false
-  }
 }
