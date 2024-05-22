29e30647945cfb7b493d258cac30d9d86ed03a94
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-23 00:51:10 +0530

    update

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 
 provider "google" {
-  credentials = file(".gcp/credentials.json")
+  credentials = file("credentials.json")
   project     = "lumen-b-ctl-047"
 }
 
 resource "google_compute_instance" "default" {
- name         = "demo-vm"
- machine_type = "e2-medium"
- zone         = "us-central1-a"
+  name         = "demo-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
  boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
- }
- }
- network_interface {
+    }
+  }
+  network_interface {
     network = "default"
- }
+  }
 }
