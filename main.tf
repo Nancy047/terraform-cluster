8f87b0e37d4108cee118c8cf7c70eac8e2ac4ac4
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-22 22:43:39 +0530

    update

--- main.tf
+++ main.tf
@@ -2,51 +2,20 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      version = "5.22.0"
     }
   }
 }
 
 provider "google" {
-  project = "LUMEN-B-CTL-047"
+  credentials_file = "credentials.json"
+  project = "lumen-b-ctl-047"
 }
 
-resource "google_container_cluster" "primary" {
-  name     = "demo-cluster"
-  location = "us-central1"
-
-  networking_mode = "VPC_NATIVE"
-  initial_node_count = 1
-  
-  remove_default_node_pool = true
-
-  autoscaling {
-    enabled = true
-  }
-}
-
-resource "google_container_node_pool" "pool_1" {
-  name       = "pool-1"
-  location  = "us-central1"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = true
-    min_nodes = 1
-    max_nodes = 3
-  }
-}
-
-resource "google_container_node_pool" "pool_2" {
-  name       = "pool-2"
-  location  = "us-central1"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = true
-    min_nodes = 1
-    max_nodes = 3
-  }
+resource "google_storage_bucket" "bucket" {
+  name          = "my-bucket"
+  location      = "US-CENTRAL1"
+  force_destroy = false
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
 }
