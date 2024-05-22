ab89de2c0226cce6cf111dc82a97a04d7a03680b
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-22 22:44:34 +0530

    update

--- main.tf
+++ main.tf
@@ -1,21 +1,27 @@
 
+resource "google_compute_instance" "default" {
+  name         = "demo-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+ boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
+
 terraform {
   required_providers {
     google = {
-      version = "5.22.0"
+      source = "hashicorp/google"
     }
   }
 }
 
 provider "google" {
-  credentials_file = "credentials.json"
-  project = "lumen-b-ctl-047"
-}
-
-resource "google_storage_bucket" "bucket" {
-  name          = "my-bucket"
-  location      = "US-CENTRAL1"
-  force_destroy = false
-  storage_class = "STANDARD"
-  uniform_bucket_level_access = true
+  credentials = file("/path/to/credentials.json")
+  project     = "lumen-b-ctl-047"
 }
