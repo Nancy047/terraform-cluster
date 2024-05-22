ad795106db0050192d4853073d232de51ba0cbe2
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-22 23:39:11 +0530

    update

--- main.tf
+++ main.tf
@@ -1,27 +1,19 @@
 
+provider "google" {
+  credentials = file("credentials.json")
+  project     = "lumen-b-ctl-047"
+}
+
 resource "google_compute_instance" "default" {
   name         = "demo-vm"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
+
  boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
     }
   }
-  network_interface {
-    network = "default"
-  }
-}
 
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
-}
-
-provider "google" {
-  credentials = file("/path/to/credentials.json")
-  project     = "lumen-b-ctl-047"
+  network_interface {}
 }
