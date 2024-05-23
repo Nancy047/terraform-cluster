22eafb83d91836ce2369d9aebf4f67c818cc5ec8
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-23 19:15:50 +0530

    update

--- main.tf
+++ main.tf
@@ -11,40 +11,35 @@ terraform {
 resource "google_container_cluster" "primary" {
   name     = "my-cluster"
   location = "us-central1"
- project  = "lumen"
+  project  = "lumen-b-ctl-047"
+
+  networking_mode = "SYSTEM_DEFAULT"
 
   remove_default_node_pool = true
-  initial_node_count       = 1
+
+  initial_node_count = 1
+}
+
+resource "google_container_node_pool" "nodes_pool_0" {
+  name       = "pool-0"
+  location  = "us-central1"
+  project   = "lumen-b-ctl-047"
+  cluster   = google_container_cluster.primary.name
+  node_count = 1
 
   autoscaling {
-    enabled = true
+    enabled = false
   }
 }
 
-resource "google_container_node_pool" "pool_1" {
+resource "google_container_node_pool" "nodes_pool_1" {
   name       = "pool-1"
   location  = "us-central1"
-  project   = "lumen"
+  project   = "lumen-b-ctl-047"
   cluster   = google_container_cluster.primary.name
   node_count = 1
 
   autoscaling {
-    enabled = true
-    min_nodes = 1
-    max_nodes = 3
-  }
-}
-
-resource "google_container_node_pool" "pool_2" {
-  name       = "pool-2"
-  location  = "us-central1"
-  project   = "lumen"
-  cluster   = google_container_cluster.primary.name
-  node_count = 1
-
-  autoscaling {
-    enabled = true
-    min_nodes = 1
-    max_nodes = 3
+    enabled = false
   }
 }
