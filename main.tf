a63b9e2573e6080a38659b7be129bb123b742d52
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-23 12:30:28 +0530

    update

--- main.tf
+++ main.tf
@@ -7,21 +7,41 @@ terraform {
   }
 }
 
-provider "google" {
-  project = "lumen-b-ctl-047"
-}
-
 resource "google_container_cluster" "primary" {
-  name     = "gke-cluster-name"
+  name     = "my-cluster"
   location = "us-central1"
-
-  initial_node_pool {
-    name               = "default-pool"
-    autoscaling        = true
-    min_node_count     = 1
-    max_node_count     = 3
-    node_locations = ["us-central1-a", "us-central1-b", "us-central1-c"]
-  }
+ project  = "lumen-b-ctl-047"
 
   remove_default_node_pool = true
+  initial_node_count       = 1
+
+  networking {
+    network = "projects/lumen-b-ctl-047/global/networks/default"
+  }
+}
+
+resource "google_container_node_pool" "nodes" {
+  name       = "pool-01"
+  location  = "us-central1"
+  project   = "lumen-b-ctl-047"
+  cluster   = google_container_cluster.primary.name
+
+  node_count = 1
+
+  autoscaling {
+    enabled = false
+  }
+}
+
+resource "google_container_node_pool" "nodes-2" {
+  name       = "pool-02"
+  location  = "us-central1"
+  project   = "lumen-b-ctl-047"
+  cluster   = google_container_cluster.primary.name
+
+  node_count = 1
+
+  autoscaling {
+    enabled = false
+  }
 }
