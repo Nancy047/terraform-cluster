2a0b7535cc689da6192d0d43876e37de0a04973e
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-27 15:22:18 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -1,28 +1,29 @@
 
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "google" {
-  credentials = file("key.json")
-  project     = "lumen-b-ctl-047"
+  credentials_file = "path/to/credentials.json"
+  project     = "sampleproject"
 }
 
-resource "google_compute_instance" "default" {
-  name         = "demo-vm"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
- boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
+resource "google_container_cluster" "primary" {
+  name     = "test-cluster"
+  location = "us-central1-c"
+
+  initial_node_count = 2
+
+ node_config {
+    machine_type = "e2-medium"
   }
-  network_interface {
-    network = "default"
+  remove_default_node_pool = true
+
+}
+
+resource "google_container_node_pool" "nodes" {
+  name       = "primary-nodes"
+  location  = "us-central1-c"
+  cluster   = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
   }
 }
