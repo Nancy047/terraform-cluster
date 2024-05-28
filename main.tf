7c8d2ff4337591cc93483606092e5773a5e0ba32
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-28 22:38:42 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -9,22 +9,15 @@ terraform {
 }
 
 provider "google" {
-  credentials = "path/to/credentials.json"
+  credentials = file("path/to/credentials.json")
   project     = "my-project"
 }
 
-resource "google_compute_instance" "default" {
-  name         = "my-vm"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
+resource "google_container_cluster" "default" {
+  name     = "my-cluster"
+  location = "us-central1-c"
+  initial_node_count = 2
+  node_config {
+    machine_type = "e2-medium"
   }
 }
