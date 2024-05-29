d9662db7c3be8be41d4181a4bf52366941c4ec1c
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-29 13:40:26 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -9,15 +9,13 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("path/to/credentials.json")
+  credentials = "key.json"
   project     = "my-project"
 }
 
-resource "google_container_cluster" "default" {
-  name     = "my-cluster"
-  location = "us-central1-c"
-  initial_node_count = 2
-  node_config {
-    machine_type = "e2-medium"
-  }
+resource "google_storage_bucket" "default" {
+  name           = "my-bucket"
+  location       = "us-central1"
+  storage_class  = "STANDARD"
+  uniform_bucket_level_access = true
 }
