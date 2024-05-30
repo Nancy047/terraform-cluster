73f27a621ee42de7c4b9518a866982d141896a0a
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-30 13:24:00 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -10,13 +10,15 @@ terraform {
 
 provider "google" {
   credentials = "key.json"
-  project     = "testproject"
+  project     = "my-project"
 }
 
 resource "google_container_cluster" "default" {
-  name     = "test-cluster"
+  name     = "my-cluster"
   location = "us-central1-c"
+
   initial_node_count = 2
+
   node_config {
     machine_type = "e2-medium"
   }
