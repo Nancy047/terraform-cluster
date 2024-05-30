a210f2c898394239ec239784d386281e11408013
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-30 22:14:37 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -13,11 +13,18 @@ provider "google" {
   project     = "my-project"
 }
 
-resource "google_container_cluster" "default" {
-  name     = "my-cluster"
-  location = "us-central1-c"
-  initial_node_count = 2
-  node_config {
-    machine_type = "e2-medium"
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
   }
 }
