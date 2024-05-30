0cf6aec09d9f2383b0f06e025d73ce98e63cec25
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-30 13:43:18 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "centos-cloud/centos-7"
     }
   }
 
