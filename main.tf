597a9f829b7d73ff7c8763d8456ec134c4521cc7
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-05-30 13:17:25 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ resource "google_container_cluster" "default" {
   name     = "test-cluster"
   location = "us-central1-c"
   initial_node_count = 2
-
   node_config {
     machine_type = "e2-medium"
   }
