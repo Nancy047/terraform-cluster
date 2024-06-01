7998058b420ccc7f62a6f1515247605f970069af
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-06-01 18:28:58 +0530

    commit made by backend api

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
 
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "keys.json"
+  project     = "lumen-b-ctl-047"
+}
+
+resource "google_container_cluster" "default" {
+  name     = "sample-cluster"
+  location = "us-central1-c"
+  initial_node_count = 2
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
