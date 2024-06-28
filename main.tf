c871a802781a5af1735e6e37ae3db0fabdde3458
terraAI <terraAI@example.com>
2024-06-28 10:50:52 +0000

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -9,12 +9,34 @@ terraform {
 }
 
 provider "google" {
-  credentials = "keys.json"
-  project     = "lumen-b-ctl-047"
+  credentials = file("keys.json")
+  project     = "abc"
 }
 
-resource "google_artifact_registry_repository" "default" {
+resource "google_pubsub_topic" "default" {
+  name     = "pubsub-topic-basic"
+  project  = "abc"
   location = "us-central1"
-  name     = "my-registry"
-  project  = "lumen-b-ctl-047"
+}
+
+resource "google_cloudfunctions_function" "default" {
+  name     = "cloud-function-basic"
+  runtime  = "nodejs16"
+  entry_point = "helloHTTP"
+  source_archive_bucket = "gs://[bucket_name]"
+  source_archive_object = "[object_name]"
+  trigger_http = true
+  project  = "abc"
+  region   = "us-central1"
+}
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "cloud-run-basic"
+  location = "us-central1"
+  template {
+    containers {
+      image = "us-docker.pkg.dev/[project_id]/[repo_name]/[image_name]"
+    }
+  }
+  project = "abc"
 }
