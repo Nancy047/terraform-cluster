bbef8e62658265bb85db02f9d36b75cb2b011031
terraAI <terraAI@example.com>
2024-07-08 12:39:08 +0000

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -9,12 +9,36 @@ terraform {
 }
 
 provider "google" {
-  credentials = "keys.json"
-  project     = "lumen-b-ctl-047"
+  credentials = file("keys.json")
+  project     = "abc"
 }
 
-resource "google_artifact_registry_repository" "default" {
+resource "google_container_cluster" "default" {
+  name     = "gcp-kubernetes-cluster"
   location = "us-central1"
-  name     = "my-registry"
-  project  = "lumen-b-ctl-047"
+  initial_node_count = 1
+  node_config {
+    machine_type = "e2-medium"
+  }
+  min_master_version = "1.19.10-gke.1000"
+  master_auth {
+    username = "admin"
+  }
+  network = "default"
+  subnetwork = "projects/gcp-project-id/regions/us-central1/subnetworks/default"
+}
+
+resource "google_cloudfunctions_function" "default" {
+  name     = "gcp-cloud-function"
+  runtime  = "nodejs16"
+  entry_point = "helloHTTP"
+  source_archive_bucket = "gcp-cloud-function-bucket"
+  source_archive_object = "gcp-cloud-function.zip"
+  trigger_http = true
+  region = "us-central1"
+}
+
+resource "google_data_lineage_domain" "default" {
+  name     = "gcp-data-lineage-domain"
+  location = "us-central1"
 }
