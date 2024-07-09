eea1750e6a3477005700e53aaaf53710387eb89c
terraAI <terraAI@example.com>
2024-07-09 10:33:05 +0000

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -9,12 +9,48 @@ terraform {
 }
 
 provider "google" {
-  credentials = "keys.json"
-  project     = "lumen-b-ctl-047"
+  credentials = file("keys.json")
+  project     = "abc"
 }
 
-resource "google_artifact_registry_repository" "default" {
+resource "google_container_cluster" "default" {
+  name     = "[cluster_name]"
   location = "us-central1"
-  name     = "my-registry"
-  project  = "lumen-b-ctl-047"
+  initial_node_count = 1
+  node_config {
+    machine_type = "e2-medium"
+  }
+  min_master_version = "1.19.10-gke.1000"
+  master_auth {
+    username = "admin"
+  }
+  network = "default"
+  subnetwork = "projects/abc/regions/us-central1/subnetworks/default"
+}
+
+resource "google_cloudfunctions_function" "default" {
+  name     = "[function_name]"
+  runtime  = "nodejs16"
+  entry_point = "helloHTTP"
+  source_archive_bucket = "[bucket_name]"
+  source_archive_object = "[object_name]"
+  trigger_http = true
+  region = "us-central1"
+}
+
+resource "google_app_engine_application" "default" {
+  location_id = "us-central1"
+  project = "abc"
+}
+
+resource "google_app_engine_service" "default" {
+  name     = "[service_name]"
+  location = "us-central1"
+  application = google_app_engine_application.default.name
+  runtime = "nodejs16"
+  entry_point = "app.js"
+  env = "standard"
+  scaling {
+    max_instances = 1
+  }
 }
