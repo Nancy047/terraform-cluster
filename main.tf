2322f1d348567032f697e4eb239dd9b4bbc9f2eb
terraAI <terraAI@example.com>
2024-07-26 04:36:44 +0000

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -9,12 +9,63 @@ terraform {
 }
 
 provider "google" {
-  credentials = "keys.json"
-  project     = "lumen-b-ctl-047"
+  credentials = file("keys.json")
+  project     = "abc"
 }
 
-resource "google_artifact_registry_repository" "default" {
+resource "google_compute_instance" "default" {
+  name         = "[vm_name]"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
+
+resource "google_apigee_instance" "default" {
+  name     = "[apigee_instance_name]"
   location = "us-central1"
-  name     = "my-registry"
-  project  = "lumen-b-ctl-047"
+  environment {
+    name = "test"
+  }
+  size = "evaluation"
+}
+
+resource "google_container_cluster" "default" {
+  name     = "[kubernetes_cluster_name]"
+  location = "us-central1-a"
+  initial_node_count = 1
+  node_config {
+    machine_type = "e2-medium"
+  }
+  master_auth {
+    username = "admin"
+  }
+}
+
+resource "google_storage_bucket" "default" {
+  name     = "[storage_bucket_name]"
+  location = "US"
+  force_destroy = true
+}
+
+resource "google_batch_job" "default" {
+  name     = "[batch_job_name]"
+  location = "us-central1"
+  job {
+    task {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      command = ["echo", "Hello, world!"]
+    }
+  }
+}
+
+resource "google_sql_database" "default" {
+  name     = "[sql_database_name]"
+  instance = "[sql_instance_name]"
 }
