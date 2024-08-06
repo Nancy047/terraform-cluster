0d1d147664a925c566346ca3d62d3f252bca388e
terraAI <terraAI@example.com>
2024-08-06 11:27:29 +0000

    Commit made by the backend process

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 
+# Configure the Google Cloud Provider
 terraform {
   required_providers {
     google = {
@@ -9,12 +10,31 @@ terraform {
 }
 
 provider "google" {
-  credentials = "keys.json"
-  project     = "lumen-b-ctl-047"
+  credentials = file("keys.json")
+  project     = "abc"
 }
 
-resource "google_artifact_registry_repository" "default" {
-  location = "us-central1"
-  name     = "my-registry"
-  project  = "lumen-b-ctl-047"
+# Deep Learning VM Image
+resource "google_compute_instance" "deep_learning_vm" {
+  name         = "deep-learning-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+  metadata = {
+    "startup-script" = <<EOF
+#!/bin/bash
+# Install necessary packages for deep learning
+yum update -y
+yum install -y python3 python3-pip
+pip3 install tensorflow keras
+# Your deep learning code goes here
+EOF
+  }
 }
